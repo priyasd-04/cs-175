cb6511fbda95e7f1b86f0caa6faf60e9d80c46a1
Davin Makris <dxmakris@uci.edu>
2026-02-04 11:13:29 -0800

    Added boilerplate code for out datasets
    Added a function that lets us load in the old english dataset, along with choosing split sizes.

---
+++ load_datasets.py
@@ -0,0 +1,25 @@
+import datasets
+from sklearn.model_selection import train_test_split
+
+
+def load_old_english_dataset(test_size:float = 0.1, val_size:float = 0.1, /, seed:int|None = None):
+    """
+    test_size: Test size split [0-1.0]
+    val_size: Val size split [0-1.0]
+    seed: randomstate seed 
+
+    Returns: (train_dataset, validation_dataset, test_dataset)
+    """
+    assert (test_size>=0 and val_size>=0 and test_size+val_size<=1)
+    ds = datasets.load_dataset("apssg96/the-old-english-dataset",
+                                split=datasets.Split.TRAIN, #Dataset only has one split: "train"
+                              )['translation'] 
+    train_ds, _ds  = train_test_split(ds, test_size=(test_size + val_size), train_size=1-(test_size + val_size),
+                                      shuffle=True, random_state=seed)
+    val_ds, test_ds = train_test_split(_ds, test_size=test_size/(test_size+val_size), train_size=val_size/(test_size+val_size),
+                                        shuffle=True, random_state=seed)
+
+    return train_ds, val_ds, test_ds
+
+train_ds, val_ds, test_ds = load_old_english_dataset()
+print("Train: ", len(train_ds), "Val: ", len(val_ds), "Test ", len(test_ds))
